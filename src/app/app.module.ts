121b0e9f5f6ecf5446e5e5e127f93575e7395a95
Eric Zumbahlen <ericzumbahlen@gmail.com>
2019-03-20 11:50:00 -0500

    trying to add get request to pull tweets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from ‘@angular/common/http’;
+import { HttpModule } from ‘@angular/http’;
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,
+    HttpModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { Http ,Response ,Headers, RequestOptions} from ‘@angular/http’;
+import { map } from ‘rxjs/operators’;
+
+constructor(private http: Http) { }
+
+this.http.get("",{}).pipe(map((res: Response) =>{
+var data = res.json();
+}));
 
 @Component({
   selector: 'app-tab1',
